6eaca4a7982ff982826f27081442323e4e8de49f
Jonathan Deluret <oni_luca@hotmail.com>
2021-03-07 16:27:33 -0300

    Revert "debugging mensajes de error en login"
    This reverts commit 07c804bc0e2871744139463c430dec36830c3247.

--- clases/usuario.php
+++ clases/usuario.php
@@ -23,8 +23,8 @@
 		public function login(){
 			$sql="SELECT * from usuario where usuario='$this->usuario' and clave='$this->clave'";
 			$resultado=$this->listar($sql);
-			if(!empty($resultado) && mysqli_num_rows($resultado)==1){
-				$resultado2=mysqli_fetch_assoc($resultado);
+			$resultado2=mysqli_fetch_assoc($resultado);
+			if(mysqli_num_rows($resultado)==1){
 				session_start();
 				$_SESSION['login']=$resultado2['usuario'];
 				$_SESSION['imgPerfil']=$resultado2['imagen'];
